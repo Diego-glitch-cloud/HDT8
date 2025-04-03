75ef9fff6e37e2300bb94fd241a6b44d4f7c7e83
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-04-02 19:38:41 -0600

    clase paciente

--- Paciente.java
+++ Paciente.java
@@ -1,4 +1,41 @@
 
 public class Paciente {
+
+    // Atributos
+    String nombre;
+    String sintoma;
+    char codigo;
+
+    // Constructor
+    Paciente(String nombre, String sintoma, char codigo) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigo = codigo;
+    }
+
+    // getters y setters 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSintoma() {
+        return sintoma;
+    }
+
+    public void setSintoma(String sintoma) {
+        this.sintoma = sintoma;
+    }
+    
+    public char getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(char codigo) {
+        this.codigo = codigo;
+    }
     
 }
