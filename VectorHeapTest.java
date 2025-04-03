81203bea16276f01ed5cd46b4b220cd72cbdec71
Diego-glitch-cloud <dcalderon918@gmail.com>
2025-04-03 02:29:01 -0600

    vectorHeap y tests

--- VectorHeapTest.java
+++ VectorHeapTest.java
@@ -26,29 +26,4 @@ public class VectorHeapTest {
         assertEquals(pB, heap.remove());
         assertTrue(heap.isEmpty());
     }
-    
-    @Test
-    public void testPrioridades() {
-        VectorHeap<Paciente> heap = new VectorHeap<>();
-        
-        Paciente pA = new Paciente("Test A", "Test", 'A');
-        Paciente pB = new Paciente("Test B", "Test", 'B');
-        Paciente pC = new Paciente("Test C", "Test", 'C');
-        Paciente pD = new Paciente("Test D", "Test", 'D');
-        Paciente pE = new Paciente("Test E", "Test", 'E');
-        
-        // orden inverso
-        heap.add(pE);
-        heap.add(pD);
-        heap.add(pC);
-        heap.add(pB);
-        heap.add(pA);
-        
-        // salir en orden de prioridad
-        assertEquals(pA, heap.remove());
-        assertEquals(pB, heap.remove());
-        assertEquals(pC, heap.remove());
-        assertEquals(pD, heap.remove());
-        assertEquals(pE, heap.remove());
-    }
 }
